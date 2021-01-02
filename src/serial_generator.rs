435ad243547999119e6373103708df1533ad5693
Jeff <jkanderson409@gmail.com>
2021-01-02 04:28:56 -0500

    Improve SerialGenerator with minor changes

--- src/serial_generator.rs
+++ src/serial_generator.rs
@@ -10,13 +10,11 @@ pub struct SerialGenerator<T = u32> {
     value: T,
 }
 
-impl<T: Default> SerialGenerator<T> {
+impl<T: Serial> SerialGenerator<T> {
     pub fn new() -> Self {
         Self::default()
     }
-}
 
-impl<T: Serial> SerialGenerator<T> {
     fn with_value(value: T) -> Self {
         SerialGenerator { value }
     }
@@ -32,15 +30,15 @@ impl<T: Serial> SerialGenerator<T> {
     }
 }
 
-impl<T: Default> Default for SerialGenerator<T> {
+impl<T: Serial> Default for SerialGenerator<T> {
     fn default() -> Self {
-        SerialGenerator { value: T::default() }
+        SerialGenerator { value: T::INIT }
     }
 }
 
 impl<T: fmt::Debug + Display> Display for SerialGenerator<T> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "{:?}", self)
+        write!(f, "{:#?}", self)
     }
 }
 
@@ -48,6 +46,8 @@ impl<T: Serial> Iterator for SerialGenerator<T> {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        Some(self.generate())
+        let next_value = self.generate();
+
+        Some(next_value)
     }
 }
