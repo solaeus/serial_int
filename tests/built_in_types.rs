938d93dd17d58602e392846d1de08a481c52afa3
Jeff <jkanderson409@gmail.com>
2021-01-15 12:09:48 -0500

    Reformat test file

--- tests/built_in_types.rs
+++ tests/built_in_types.rs
@@ -24,10 +24,19 @@ fn recreates_end_value<T: Serial + std::fmt::Debug>(init: T, end: T) {
     assert_eq!(end, gen.generate());
 }
 
+/* u8 */
+
 #[test]
 fn u8_creates_unique_values_until_end() {
     creates_unique_values_until_end(u8::MAX - 5);
 }
+
+#[test]
+fn u8_recreates_end_value() {
+    recreates_end_value(u8::MAX - 1, u8::MAX);
+}
+
+/* u16 */
 
 #[test]
 fn u16_creates_unique_values_until_end() {
@@ -35,34 +44,15 @@ fn u16_creates_unique_values_until_end() {
 }
 
 #[test]
-fn u32_creates_unique_values_until_end() {
-    creates_unique_values_until_end(u32::MAX - 5);
-}
-
-#[test]
-fn u64_creates_unique_values_until_end() {
-    creates_unique_values_until_end(u64::MAX - 5);
-}
-
-#[test]
-fn u128_creates_unique_values_until_end() {
-    creates_unique_values_until_end(u128::MAX - 5);
-}
-
-#[test]
-fn usize_creates_unique_values_until_end() {
-    creates_unique_values_until_end(usize::MAX - 5);
-}
-
-
-#[test]
-fn u8_recreates_end_value() {
-    recreates_end_value(u8::MAX - 1, u8::MAX);
-}
-
-#[test]
 fn u16_recreates_end_value() {
     recreates_end_value(u16::MAX - 1, u16::MAX);
+}
+
+/* u32 */
+
+#[test]
+fn u32_creates_unique_values_until_end() {
+    creates_unique_values_until_end(u32::MAX - 5);
 }
 
 #[test]
@@ -70,14 +60,35 @@ fn u32_recreates_end_value() {
     recreates_end_value(u32::MAX - 1, u32::MAX);
 }
 
+/* u64 */
+
+#[test]
+fn u64_creates_unique_values_until_end() {
+    creates_unique_values_until_end(u64::MAX - 5);
+}
+
 #[test]
 fn u64_recreates_end_value() {
     recreates_end_value(u64::MAX - 1, u64::MAX);
 }
 
+/* u128 */
+
+#[test]
+fn u128_creates_unique_values_until_end() {
+    creates_unique_values_until_end(u128::MAX - 5);
+}
+
 #[test]
 fn u128_recreates_end_value() {
     recreates_end_value(u128::MAX - 1, u128::MAX);
+}
+
+/* usize */
+
+#[test]
+fn usize_creates_unique_values_until_end() {
+    creates_unique_values_until_end(usize::MAX - 5);
 }
 
 #[test]
