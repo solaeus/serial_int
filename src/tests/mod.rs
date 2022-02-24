c4f8b9b36dcd559358faa7583599e1ba028c8025
Jeff <dev@jeffa.io>
2022-02-24 16:56:08 -0500

    Fix test; Add assert to ensure that test works correctly

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -8,13 +8,15 @@ pub fn creates_unique_values_until_end<T: Serial + Ord>(start: T) {
     let mut gen = SerialGenerator::with_init_value(start);
     let mut used = Vec::new();
 
-    while gen.is_at_max() {
+    while !gen.is_at_max() {
         let serial = gen.generate();
 
         assert!(!used.contains(&serial));
 
         used.push(serial);
     }
+
+    assert!(!used.is_empty(), "Test did not check for anything...")
 }
 
 pub fn recreates_end_value<T: Serial + std::fmt::Debug>(init: T, end: T) {
